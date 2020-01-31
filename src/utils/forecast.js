94823e452e4b43e0b7cbc742d42304f15de4dac9
SHAIKH ABDUL MAJID <samajid80@yahoo.com>
2020-01-31 19:46:08 +0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/fdbc816387a4b860d73352175c52b9cf/" +
+    latitude +
+    "," +
+    longitude +
+    "?units=si";
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to weather service.", undefined);
+    } else if (body.error) {
+      callback("Unable to find Location.", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degrees out. There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
